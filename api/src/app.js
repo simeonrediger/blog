3a2c4d05c47e4293dc600a7c61ba8d52a2ecea76
Simeon Rediger <simeonrediger@gmail.com>
2026-08-16 13:41:48 -0400

    Add 404 error middleware

--- api/src/app.js
+++ api/src/app.js
@@ -2,6 +2,10 @@ import express from 'express';
 
 const app = express();
 
+app.use((req, res) => {
+  res.status(404).json({ error: 'Resource not found' });
+});
+
 app.use((error, req, res, next) => {
   console.error(error);
   res.status(500).json({ error: 'Server error' });
