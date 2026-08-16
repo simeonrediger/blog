95fc23f0ad2ebcb079e0eef54a95532eed12f858
Simeon Rediger <simeonrediger@gmail.com>
2026-08-16 13:38:10 -0400

    Add general error middleware

--- api/src/app.js
+++ api/src/app.js
@@ -2,4 +2,9 @@ import express from 'express';
 
 const app = express();
 
+app.use((error, req, res, next) => {
+  console.error(error);
+  res.status(500).json({ error: 'Server error' });
+});
+
 export default app;
