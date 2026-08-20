bb400ee348a665d87bf745fc9f03b103c51a3ab3
Simeon Rediger <simeonrediger@gmail.com>
2026-08-19 22:24:29 -0400

    Refactor token expiration to use vercel/ms value

--- api/src/services/auth.service.js
+++ api/src/services/auth.service.js
@@ -1,9 +1,7 @@
 import jwt from 'jsonwebtoken';
 
 export async function createToken(userId) {
-  const DAY_IN_SEC = 60 * 60 * 24;
-
   return jwt.sign({ sub: userId }, process.env.JWT_SECRET, {
-    expiresIn: 30 * DAY_IN_SEC,
+    expiresIn: '30d',
   });
 }
