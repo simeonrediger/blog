c0a2b9f2de6c63f1e7aaedf8965dc08bf104ed17
Simeon Rediger <simeonrediger@gmail.com>
2026-08-19 22:21:14 -0400

    Refactor token shape and contents

--- api/src/services/auth.service.js
+++ api/src/services/auth.service.js
@@ -1,9 +1,9 @@
 import jwt from 'jsonwebtoken';
 
-export async function createToken({ username }) {
+export async function createToken(userId) {
   const DAY_IN_SEC = 60 * 60 * 24;
 
-  return jwt.sign({ username }, process.env.JWT_SECRET, {
+  return jwt.sign({ sub: userId }, process.env.JWT_SECRET, {
     expiresIn: 30 * DAY_IN_SEC,
   });
 }
