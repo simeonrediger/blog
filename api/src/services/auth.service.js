0dddd1af07ecbfdda6d3c177053a9c934d84ee16
Simeon Rediger <simeonrediger@gmail.com>
2026-08-20 23:46:09 -0400

    Change createToken to be synchronous

--- api/src/services/auth.service.js
+++ api/src/services/auth.service.js
@@ -1,6 +1,6 @@
 import jwt from 'jsonwebtoken';
 
-export async function createToken(userId) {
+export function createToken(userId) {
   return jwt.sign({ sub: userId }, process.env.JWT_SECRET, {
     expiresIn: '30d',
   });
