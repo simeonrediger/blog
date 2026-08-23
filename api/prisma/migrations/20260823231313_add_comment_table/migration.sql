d8b9bb63f43113c3c1c3a7e520ff3f7a16b49436
Simeon Rediger <simeonrediger@gmail.com>
2026-08-23 19:16:12 -0400

    Add comment table

---
+++ api/prisma/migrations/20260823231313_add_comment_table/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "Comment" (
+    "id" SERIAL NOT NULL,
+    "authorName" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "postId" INTEGER NOT NULL,
+
+    CONSTRAINT "Comment_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Comment" ADD CONSTRAINT "Comment_postId_fkey" FOREIGN KEY ("postId") REFERENCES "Post"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
