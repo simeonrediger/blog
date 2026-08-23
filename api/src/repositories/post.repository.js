966d97f25fc217e7d34cee2c56e07dcb52288cfa
Simeon Rediger <simeonrediger@gmail.com>
2026-08-23 17:26:47 -0400

    Reorder function declarations

--- api/src/repositories/post.repository.js
+++ api/src/repositories/post.repository.js
@@ -1,5 +1,9 @@
 import prisma from '../db/prisma.js';
 
+export async function findById(id) {
+  return await prisma.post.findUnique({ where: { id } });
+}
+
 export async function create({ title, content, authorId }) {
   return await prisma.post.create({
     data: { title, content, authorId },
@@ -8,10 +12,6 @@ export async function create({ title, content, authorId }) {
       author: { include: { passwordHash: false } },
     },
   });
-}
-
-export async function findById(id) {
-  return await prisma.post.findUnique({ where: { id } });
 }
 
 export async function update({ id, title, content }) {
