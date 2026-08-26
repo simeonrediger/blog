34dd54ee9b82a9820a8838fbb0486c236c82aec0
Simeon Rediger <simeonrediger@gmail.com>
2026-08-26 16:46:08 -0400

    Add header with page title

--- apps/blog/src/App/App.jsx
+++ apps/blog/src/App/App.jsx
@@ -3,6 +3,9 @@ import { Outlet } from 'react-router';
 export default function App() {
   return (
     <div>
+      <header>
+        <h1>Blog</h1>
+      </header>
       <main>
         <Outlet />
       </main>
