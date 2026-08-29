569ce6d4bd994026f19b558da771122903e93dde
Simeon Rediger <simeonrediger@gmail.com>
2026-08-29 17:17:02 -0400

    Wrap title in link to post

--- apps/blog/src/routes/PostList/PostListItem/PostListItem.jsx
+++ apps/blog/src/routes/PostList/PostListItem/PostListItem.jsx
@@ -1,9 +1,13 @@
+import { Link } from 'react-router';
+
 import styles from './PostListItem.module.css';
 
-export default function PostListItem({ title, createdAt, author }) {
+export default function PostListItem({ id, title, createdAt, author }) {
   return (
     <article className={styles.listItem}>
-      <h3>{title}</h3>
+      <h3>
+        <Link to={`/posts/${id}`}>{title}</Link>
+      </h3>
       <p>by {author.username}</p>
       <p className={styles.createdAt}>{new Date(createdAt).toLocaleString()}</p>
     </article>
