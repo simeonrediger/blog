3573e0f1e789b699278381ed62d7838fbcb1b296
Simeon Rediger <simeonrediger@gmail.com>
2026-08-30 17:50:46 -0400

    Add timestamps to post

--- apps/blog/src/routes/Post/Post.jsx
+++ apps/blog/src/routes/Post/Post.jsx
@@ -3,6 +3,7 @@ import { useParams } from 'react-router';
 import api from '../../api-client/client.js';
 import useFetch from '../../hooks/useFetch.js';
 
+import DateTime from '../../components/DateTime/DateTime.jsx';
 import ErrorPage from '../ErrorPage/ErrorPage.jsx';
 import PageLoader from '../../components/PageLoader/PageLoader.jsx';
 
@@ -18,12 +19,21 @@ export default function Post() {
     return <PageLoader />;
   }
 
-  const { title, content, author } = data.post;
+  const { title, content, createdAt, editedAt, author } = data.post;
 
   return (
     <section>
       <h2>{title}</h2>
       <p>by {author.username}</p>
+      <p>
+        <DateTime value={createdAt} />
+        {editedAt !== createdAt && (
+          <>
+            {' '}
+            (edited <DateTime value={editedAt} />)
+          </>
+        )}
+      </p>
       <p>{content}</p>
     </section>
   );
