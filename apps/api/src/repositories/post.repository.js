f594614baec42c73014d4e46e428822013f8728b
Simeon Rediger <simeonrediger@gmail.com>
2026-08-30 16:12:15 -0400

    Include author in findById

--- apps/api/src/repositories/post.repository.js
+++ apps/api/src/repositories/post.repository.js
@@ -3,7 +3,7 @@ import prisma from '../db/prisma.js';
 export async function findById(id) {
   return await prisma.post.findUnique({
     where: { id },
-    include: { comments: true },
+    include: { author: true, comments: true },
   });
 }
 
