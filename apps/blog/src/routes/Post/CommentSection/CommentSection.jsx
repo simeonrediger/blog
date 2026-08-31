25556553da7b0c623836d53e694ea2b7c3bbbf4c
Simeon Rediger <simeonrediger@gmail.com>
2026-08-30 22:37:50 -0400

    Add block margin to comment section

--- apps/blog/src/routes/Post/CommentSection/CommentSection.jsx
+++ apps/blog/src/routes/Post/CommentSection/CommentSection.jsx
@@ -1,8 +1,9 @@
+import styles from './CommentSection.module.css';
 import CommentList from './CommentList/CommentList.jsx';
 
 export default function CommentSection({ comments }) {
   return (
-    <section>
+    <section className={styles.commentSection}>
       <h2>Comments</h2>
       <CommentList comments={comments} />
     </section>
