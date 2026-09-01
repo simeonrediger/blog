13448bff6fb9961447a8fa06201d5da70184b6d2
Simeon Rediger <simeonrediger@gmail.com>
2026-09-01 18:51:22 -0400

    Set max width of content

--- apps/blog/src/App/App.jsx
+++ apps/blog/src/App/App.jsx
@@ -5,19 +5,21 @@ import styles from './App.module.css';
 export default function App() {
   return (
     <div className={styles.app}>
-      <header className={styles.header}>
-        <h1 className={styles.siteTitle}>Blog</h1>
-        <nav>
-          <ul>
-            <li>
-              <NavLink to="/">Home</NavLink>
-            </li>
-          </ul>
-        </nav>
-      </header>
-      <main>
-        <Outlet />
-      </main>
+      <div className={styles.content}>
+        <header className={styles.header}>
+          <h1 className={styles.siteTitle}>Blog</h1>
+          <nav>
+            <ul>
+              <li>
+                <NavLink to="/">Home</NavLink>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <main>
+          <Outlet />
+        </main>
+      </div>
     </div>
   );
 }
