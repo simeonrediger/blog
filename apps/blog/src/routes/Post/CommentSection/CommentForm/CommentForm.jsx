775b0a8f59743b5fcce2a0a463db98ebd7ca05fa
Simeon Rediger <simeonrediger@gmail.com>
2026-09-01 15:06:54 -0400

    Rename api-utils.js to handle-submit.js

--- apps/blog/src/routes/Post/CommentSection/CommentForm/CommentForm.jsx
+++ apps/blog/src/routes/Post/CommentSection/CommentForm/CommentForm.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 
 import api from '@/api-client/client.js';
-import handleSubmit from '@/api-client/api-utils.js';
+import handleSubmit from '@/api-client/handle-submit.js';
 
 import styles from './CommentForm.module.css';
 import ErrorList from '@/components/ErrorList/ErrorList.jsx';
