aadd2779cd2a7501d1cf72106442b969f68d334e
Simeon Rediger <simeonrediger@gmail.com>
2026-09-01 15:23:10 -0400

    Extract buildApi into build-api.js

---
+++ apps/blog/src/api-client/build-api.js
@@ -0,0 +1,19 @@
+const API_ORIGIN = import.meta.env.VITE_API_ORIGIN;
+
+export default function buildApi(apiNamespace) {
+  for (const [methodName, { path, options }] of Object.entries(apiNamespace)) {
+    apiNamespace[methodName] = createApiMethod(path, options);
+  }
+}
+
+function createApiMethod(path, baseOptions) {
+  return typeof path === 'function'
+    ? (params, options = {}) => {
+        Object.assign(options, baseOptions);
+        return fetch(`${API_ORIGIN}${path(params)}`, options);
+      }
+    : (options = {}) => {
+        Object.assign(options, baseOptions);
+        return fetch(`${API_ORIGIN}${path}`, options);
+      };
+}
--- apps/blog/src/api-client/client.js
+++ apps/blog/src/api-client/client.js
@@ -1,4 +1,4 @@
-const API_ORIGIN = import.meta.env.VITE_API_ORIGIN;
+import buildApi from './build-api.js';
 
 const api = {
   posts: {
@@ -15,23 +15,5 @@ const api = {
 };
 
 buildApi(api.posts);
-
-function buildApi(apiNamespace) {
-  for (const [methodName, { path, options }] of Object.entries(apiNamespace)) {
-    apiNamespace[methodName] = createApiMethod(path, options);
-  }
-}
-
-function createApiMethod(path, baseOptions) {
-  return typeof path === 'function'
-    ? (params, options = {}) => {
-        Object.assign(options, baseOptions);
-        return fetch(`${API_ORIGIN}${path(params)}`, options);
-      }
-    : (options = {}) => {
-        Object.assign(options, baseOptions);
-        return fetch(`${API_ORIGIN}${path}`, options);
-      };
-}
 
 export default api;
