7cb0a142c2e5d23bd2a1654207fdcec15568649d
Simeon Rediger <simeonrediger@gmail.com>
2026-09-09 01:51:01 -0400

    Reorder imports

--- apps/blog/src/App/App.jsx
+++ apps/blog/src/App/App.jsx
@@ -1,8 +1,9 @@
 import { Outlet } from 'react-router';
 
+import PermissionsProvider from '@/context/PermissionsProvider.jsx';
+
 import styles from './App.module.css';
 import Header from './Header/Header.jsx';
-import PermissionsProvider from '@/context/PermissionsProvider.jsx';
 
 export default function App() {
   return (
