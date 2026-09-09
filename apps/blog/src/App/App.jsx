0362586ca7fb42b08f372b0a09900d4854d62429
Simeon Rediger <simeonrediger@gmail.com>
2026-09-09 02:19:43 -0400

    Extract BaseApp from App

--- apps/blog/src/App/App.jsx
+++ apps/blog/src/App/App.jsx
@@ -1,24 +1,14 @@
-import { Outlet } from 'react-router';
-
 import api from '@/api-client.js';
 import ApiProvider from '@/context/api/ApiProvider.jsx';
 import PermissionsProvider from '@/context/permissions/PermissionsProvider.jsx';
 
-import styles from './App.module.css';
-import Header from './Header/Header.jsx';
+import BaseApp from './BaseApp/BaseApp.jsx';
 
 export default function App() {
   return (
     <ApiProvider api={api}>
       <PermissionsProvider>
-        <div className={styles.app}>
-          <div className={styles.content}>
-            <Header />
-            <main>
-              <Outlet />
-            </main>
-          </div>
-        </div>
+        <BaseApp />
       </PermissionsProvider>
     </ApiProvider>
   );
---
+++ apps/blog/src/App/BaseApp/BaseApp.jsx
@@ -0,0 +1,17 @@
+import { Outlet } from 'react-router';
+
+import styles from './BaseApp.module.css';
+import Header from '../Header/Header.jsx';
+
+export default function BaseApp() {
+  return (
+    <div className={styles.app}>
+      <div className={styles.content}>
+        <Header />
+        <main>
+          <Outlet />
+        </main>
+      </div>
+    </div>
+  );
+}
